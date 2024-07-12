3c85c57deb9f0e94505f37374193a744ec8bb790
Jalil <kechekar1995@gmail.com>
2024-07-12 15:43:48 +1000

    first push
    first push

---
+++ Scenes/Ball.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+var ball_speed = 200
+var ball_accel = 50
+var ball_movement : Vector2
+
+func _ready():
+	position.x = 0
+	position.y = randi_range(200, -200)
+	ball_movement = random_direction()
+		
+		
+		
+func random_direction():
+	var new_movement = Vector2()
+	new_movement.x = [1, -1].pick_random()
+	new_movement.y = randf_range(-1, 1)
+	return new_movement.normalized()
+	
+func _physics_process(delta):
+	var collision = move_and_collide(ball_movement * ball_speed * delta)
+	var collider
+	if collision:
+		collider = collision.get_collider()
+		if collider == $"../Player" or collider == $"../Computer":
+			ball_speed += ball_accel
+			ball_movement = ball_movement.bounce(collision.get_normal())
+		else:
+			ball_movement = ball_movement.bounce(collision.get_normal())
+	
---
+++ Scripts/Player_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+
+func _physics_process(delta):
+	var input = Vector2()
+	 
+	if Input.is_action_pressed("up"):
+		input.y -= 1
+	if Input.is_action_pressed("down"):
+		input.y += 1
+		
+		
+		
+	velocity = input * speed
+	input.normalized()
+	input = move_and_slide()
